3b9d768b92fae0b1ae3555695851550354a285bd
lewster32 <septimus@rotates.org>
2014-08-03 20:54:18 +0100

    Added more methods to Point3.

--- src/geom/Point3.js
+++ src/geom/Point3.js
@@ -26,6 +26,19 @@ Phaser.Point3 = function (x, y, z) {
 
 Phaser.Point3.prototype = {
     /**
+    * Copies the x, y and z properties from any given object to this Point3.
+    *
+    * @method Phaser.Point3#copyFrom
+    * @param {any} source - The object to copy from.
+    * @return {Phaser.Point3} This Point3 object.
+    */
+    copyFrom: function (source) {
+
+        return this.setTo(source.x, source.y, source.z);
+
+    },
+
+    /**
     * Sets the x, y and z values of this Point3 object to the given values.
     * If you omit the y and z value then the x value will be applied to all three, for example:
     * `Point3.set(2)` is the same as `Point3.set(2, 2, 2)`
@@ -59,5 +72,79 @@ Phaser.Point3.prototype = {
     */
     setTo: function (x, y, z) {
         return this.set(x, y, z);
-    }
+    },
+
+    /**
+    * Adds the given x, y and z values to this Point3.
+    *
+    * @method Phaser.Point3#add
+    * @param {number} x - The value to add to Point3.x.
+    * @param {number} y - The value to add to Point3.y.
+    * @param {number} z - The value to add to Point3.z.
+    * @return {Phaser.Point3} This Point3 object. Useful for chaining method calls.
+    */
+    add: function (x, y) {
+
+        this.x += x || 0;
+        this.y += y || 0;
+        return this;
+
+    },
+
+    /**
+    * Subtracts the given x, y and z values from this Point3.
+    *
+    * @method Phaser.Point3#subtract
+    * @param {number} x - The value to subtract from Point3.x.
+    * @param {number} y - The value to subtract from Point3.y.
+    * @param {number} z - The value to subtract from Point3.z.
+    * @return {Phaser.Point3} This Point3 object. Useful for chaining method calls.
+    */
+    subtract: function (x, y, z) {
+
+        this.x -= x || 0;
+        this.y -= y || 0;
+        this.z -= z || 0;
+
+        return this;
+
+    },
+
+    /**
+    * Multiplies Point3.x, Point3.y and Point3.z by the given x and y values. Sometimes known as `Scale`.
+    *
+    * @method Phaser.Point3#multiply
+    * @param {number} x - The value to multiply Point3.x by.
+    * @param {number} y - The value to multiply Point3.y by.
+    * @param {number} z - The value to multiply Point3.z by.
+    * @return {Phaser.Point3} This Point object. Useful for chaining method calls.
+    */
+    multiply: function (x, y, z) {
+
+        this.x *= x || 1;
+        this.y *= y || 1;
+        this.z *= z || 1;
+
+        return this;
+
+    },
+
+    /**
+    * Divides Point3.x, Point3.y and Point3.z by the given x, y and z values.
+    *
+    * @method Phaser.Point3#divide
+    * @param {number} x - The value to divide Point3.x by.
+    * @param {number} y - The value to divide Point3.y by.
+    * @param {number} z - The value to divide Point3.z by.
+    * @return {Phaser.Point3} This Point3 object. Useful for chaining method calls.
+    */
+    divide: function (x, y, z) {
+
+        this.x /= x || 1;
+        this.y /= y || 1;
+        this.z /= z || 1;
+
+        return this;
+
+    },
 };
